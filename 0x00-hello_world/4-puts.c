4319842722c1b28697136cc45f34280adcf1ddc3
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-10 17:23:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    // Use the puts function to print the desired message
-    puts("Programming is like building a multilingual puzzle,");
     
-    // Return 0 to indicate successful execution
+    puts("Programming is like building a multilingual puzzle");
+    
     return 0;
 }
 
