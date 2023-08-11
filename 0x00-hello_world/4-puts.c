677e0fac95f1b8d402f060fa5d90bc924c8768f7
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-10 17:30:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: Prints the message "Programming is like building a multilingual puzzle"
+ * Description: Prints the message "Programming is like building a multilingual puzzle
  *              followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
