dc5647357596f4cbdaffdf9a56338ba02a8c0f6f
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-15 12:56:39 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,8 @@
 /*
  * main - Entry point
  *
- * Description: Write a program that prints _putchar, followed by a new line.
+ * Description: Prints "_putchar" followed by a new line
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
