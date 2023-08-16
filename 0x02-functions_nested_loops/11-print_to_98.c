6bb0248f8f3c106960a75c69e398b0be425af203
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-15 17:04:36 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,27 +7,33 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	if (n < 98)
-	{
-		for (i == n; i <= 98;i++)
-		{
-			_putchar(i);
-			_putchar(',');
-			_putchar(" ");
-		}
-	}
-
-	else if (n > 98)
-	{
-		for (i == n; i <= 98;i--)
-		{
-			_putchar(i);
-			_putchar(',');
-			_putchar(" ");
-		}
-	}
-
-	else 
-	       _putchar("98");	
+    int i;
+    
+    if (n < 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            printf("%d", i);
+            if (i != 98)
+            {
+                printf(", ");
+            }
+        }
+    }
+    else if (n > 98)
+    {
+        for (i = n; i >= 98; i--)
+        {
+            printf("%d", i);
+            if (i != 98)
+            {
+                printf(", ");
+            }
+        }
+    }
+    else
+    {
+        printf("98");
+    }
+    printf("\n");
 }
