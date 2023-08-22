dc23ed41c1bfd17133cbdeef72b35e0ec6813f0c
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-22 09:37:22 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_length - Returns the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0;len <= s; le++)
+	{
+		len++;
+	}
+
+	return len;
+}
