7098b19eebd29c63ab4f89c6f9420b0cb9c443b2
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-22 10:14:53 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@
 void rev_string(char *s)
 {
 	int length = 0;
+	int start = 0;
+	int end = length - 1;
+	char temp;
+
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-
-	int start = 0;
-	int end = length - 1;
-	char temp;
 
 	while (start < end)
 	{
