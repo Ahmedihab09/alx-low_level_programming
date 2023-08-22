0505e4a3bc4a216e088e45a638c32db9558cf939
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-22 10:27:21 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,19 +2,20 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character.
+ * puts2 - Prints each character of a string, starting with the first character.
  * @str: The input string.
  */
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0') {
+	while (str[i] != '\0')
+	{
 		putchar(str[i]);
 		i++;
-		if (str[i] == '\0') {
+		if (str[i] == '\0')
 			break;
-		}
+		
 		i++;
 	}
 
