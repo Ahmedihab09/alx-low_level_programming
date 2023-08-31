367dfa2cc32fbe03baf35da96597e0e3a242c99f
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-30 18:14:01 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: The input string.
+ *
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		putchar('\n');
+		return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Hello, Recursive World!";
+	_puts_recursion(str);
+	return (0);
+}
+
