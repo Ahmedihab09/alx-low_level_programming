260fd72bf2d36265a753f659e765cef2feff08d7
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-30 18:16:51 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,26 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (!*s)
-	{
-		putchar('\n');
-		return;
-	}
+    if (!*s)
+    {
+        putchar('\n');
+        return;
+    }
 
-	putchar(*s);
-	_puts_recursion(s + 1);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char str[] = "Hello, Recursive World!";
-
-	_puts_recursion(str);
-	return (0);
+    putchar(*s);
+    _puts_recursion(s + 1);
 }
 
