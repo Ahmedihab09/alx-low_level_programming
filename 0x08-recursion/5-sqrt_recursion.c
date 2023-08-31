91bd7c7827c796aa6fec7910df9462da2f29a017
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-08-30 18:50:47 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,36 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
+    if (n < 0)
+        return -1;
 
-	if (n == 0 || n == 1)
-		return (n);
+    if (n == 0 || n == 1)
+        return n;
 
-	return (find_sqrt(n, 2));
+    return recursive_sqrt(n, 1, n);
+}
+
+/**
+ * recursive_sqrt - Recursive function to find the square root.
+ * @n: The number to calculate the square root of.
+ * @low: The lower bound of the search range.
+ * @high: The upper bound of the search range.
+ *
+ * Return: The natural square root of @n, or -1 if @n does not have a natural square root.
+ */
+int recursive_sqrt(int n, int low, int high)
+{
+    int mid = (low + high) / 2;
+    
+    if (mid * mid == n)
+        return mid;
+
+    if (mid * mid > n)
+        return recursive_sqrt(n, low, mid - 1);
+
+    if (mid * mid < n)
+        return recursive_sqrt(n, mid + 1, high);
+
+    return -1;
 }
 
