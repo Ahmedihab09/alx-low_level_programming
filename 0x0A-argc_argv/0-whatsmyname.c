5fa80ecd99fb420ca2992a1e3b13fcc6ba6145a5
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-01 16:22:31 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    if (argc > 0) {
-        printf("%s\n", argv[0]);
-    }
-    return 0;
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
 }
 
