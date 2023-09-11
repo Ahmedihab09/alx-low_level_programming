5399088e14475b34db3da42dff249b121bed8246
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-11 09:45:37 -0700

    dog.h

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - Structure representing information about a dog.
+ * @name: Name of the dog (string).
+ * @age: Age of the dog (float).
+ * @owner: Owner's name (string).
+ *
+ * Description: This structure holds information about a dog,
+ * including its name, age, and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
