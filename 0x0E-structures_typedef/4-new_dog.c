ba20a046f26157cdcb9464ddac22ccd6902eebfa
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-11 17:25:11 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <string.h>
 /**
- * new_dog - Creates a new dog and stores a copy of name and owner.
- * @name: Name of the dog (string).
- * @age: Age of the dog (float).
- * @owner: Owner's name (string).
- *
- * Return: Pointer to the newly created dog, or NULL if it fails.
- */
+ * new_dog - creates a new dog
+ * @name: name member of the dog 
+ * @age: age member of the dog 
+ * @owner: owner member of the dog 
+ * Return: pointer to the new dog_t 
+ **/
 dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *new_dog_ptr;
-	char *name_copy, *owner_copy;
+{	
+	char *temp_name;
+	char *temp_owner;
+	dog_t *new_doggo;
 
-	new_dog_ptr = malloc(sizeof(dog_t));
-	if (new_dog_ptr == NULL)
-		return (NULL);
-
-	name_copy = strdup(name);
-	owner_copy = strdup(owner);
-
-	if (name_copy == NULL || owner_copy == NULL)
+	temp_name = malloc(sizeof(*name) * (strlen(name) + 1));
+	temp_owner = malloc(sizeof(*owner) * (strlen(owner) + 1));
+	new_doggo = malloc(sizeof(dog_t));
+	if (temp_name && temp_owner && new_doggo)
 	{
-		free(new_dog_ptr);
-		free(name_copy);
-		free(owner_copy);
+		strcpy(temp_name, name);
+		strcpy(temp_owner, owner);
+		new_doggo->name = temp_name;
+		new_doggo->age = age;
+		new_doggo->owner = temp_owner;	
+	}	
+	else	
+	{	
+		free(temp_name);
+		free(temp_owner);	
+		free(new_doggo);
 		return (NULL);
 	}
-
-	new_dog_ptr->name = name_copy;
-	new_dog_ptr->age = age;
-	new_dog_ptr->owner = owner_copy;
-
-	return (new_dog_ptr);
+	return (new_doggo);
 }
