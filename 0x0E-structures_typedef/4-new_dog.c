8e65b09ef5dea837fcc516952922fe44c28b31ab
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-11 17:33:22 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,26 +13,23 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || owner == NULL)
-		return (NULL);
+    dog_t *new_dog_ptr;
+    char *name_copy, *owner_copy;
 
-	dog_t *new_dog_ptr;
-	char *name_copy, *owner_copy;
+    new_dog_ptr = malloc(sizeof(dog_t));
+    if (new_dog_ptr == NULL)
+        return (NULL);
 
-	new_dog_ptr = malloc(sizeof(dog_t));
-	if (new_dog_ptr == NULL)
-		return (NULL);
+    name_copy = strdup(name);
+    owner_copy = strdup(owner);
 
-	name_copy = strdup(name);
-	owner_copy = strdup(owner);
-
-	if (name_copy == NULL || owner_copy == NULL)
-	{
-		free(new_dog_ptr);
-		free(name_copy);
-		free(owner_copy);
-		return (NULL);
-	}
+    if (name_copy == NULL || owner_copy == NULL)
+    {
+        free(new_dog_ptr);
+        free(name_copy);
+        free(owner_copy);
+        return (NULL);
+    }
 
 	new_dog_ptr->name = name_copy;
 	new_dog_ptr->age = age;
@@ -40,5 +37,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (new_dog_ptr);
 }
-
 
