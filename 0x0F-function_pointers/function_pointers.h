c63a8dd1de92c5b9d4c085ef4fc31038afebb74c
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-13 09:15:21 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,6 @@
 
 void print_name(char *name, void (*f)(char *));
 void print_name_function(char *name);
-
+void array_iterator(int *array, size_t size, void (*action)(int));`
 #endif /* MAIN_H */
 
