1f98040776b715d50e95c77d586a25c6c19a1b3a
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-13 12:57:13 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+
+/**
+ * main - Entry point for the calculator program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 upon success, 98 for incorrect number of arguments,
+ *         99 for invalid operator, or other values as specified.
+ */
+
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
