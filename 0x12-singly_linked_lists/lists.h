62481c42a8d496937b888d2071488610b4a9ef2e
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-09-22 08:10:40 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct list_s
+{
+	char *str;
+	int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 int _putchar(char c);
 #endif /* MAIN_H */
