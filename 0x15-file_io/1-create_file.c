404bab5583b9bfb547b17cfdd05017480fea9ec8
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-10-02 13:23:57 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,15 @@
 #include <fcntl.h>
 #include <string.h>
 
+/**
+ * create_file - Creates a file with specified permissions and writes text content.
+ * @filename: The name of the file to create.
+ * @text_content: The NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ * If the file can't be created, written, or any other failure occurs, return -1.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -18,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		ssize_t bytes_written;
-			
+
 		bytes_written = write(fd, text_content, strlen(text_content));
 		if (bytes_written == -1)
 		{
