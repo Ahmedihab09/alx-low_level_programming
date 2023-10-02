a61cb57978e00c2d8e4620fb9986351311b9ace9
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-10-02 13:07:08 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,23 +13,29 @@
  * If filename is NULL, return 0.
  * If the write operation fails or doesn't write the expected amount of bytes, return 0.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t bytes_read;
+        ssize_t bytes_written;
+        char *buffer;
+
 	if (filename == NULL)
 		return (0);
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	char *buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
 
-	ssize_t bytes_read = read(fd, buffer, letters);
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
 		close(fd);
@@ -37,7 +43,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(fd);
@@ -49,3 +55,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	return (bytes_written);
 }
+
