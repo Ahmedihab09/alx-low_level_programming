b7230a4413d26e2ea5d8fa4740d533d40a0fad8d
Ahmedihab09 <ahmed.ihab353@gmail.com>
2023-10-02 13:16:19 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written; 
+			
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
